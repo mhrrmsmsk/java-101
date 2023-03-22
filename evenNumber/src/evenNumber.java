263d2c72ef477b47c9e8d42e374139d4b4f91c03
Muharrem Şimşek <101654299+mhrrmsmsk@users.noreply.github.com>
2023-03-23 01:05:31 +0300

    Add files via upload

---
+++ evenNumber/src/evenNumber.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+public class evenNumber {
+    public static void main(String[] args) {
+        int a, b = 0;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Bir sayı giriniz :");
+        a = input.nextInt();
+        for(int i=0;i<a;i++){
+            if(i%3==0 && i%4==0){
+                b +=i;
+            }
+        }
+        System.out.println("Ortalama : "+b);
+    }
+}
