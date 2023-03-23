e1c9c0a794f65f6cd79bfe3fc96f3f9a9be8c0e8
Muharrem Şimşek <101654299+mhrrmsmsk@users.noreply.github.com>
2023-03-23 22:00:19 +0300

    Add files via upload

---
+++ sumOfEvenNumber/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       int k, total=0;
+        Scanner scanner = new Scanner(System.in);
+
+        do {
+            System.out.print("Sayı giriniz:");
+            k = scanner.nextInt();
+
+            if (k%4==0){
+                total +=k;
+            }
+        }while (k%2==0);
+        System.out.println("Toplam :"+total);
+    }
+}
