5925f4c9c50f9107b550c227e7ca20914076a561
Muharrem Şimşek <101654299+mhrrmsmsk@users.noreply.github.com>
2023-03-24 17:22:23 +0300

    Add files via upload

---
+++ exponent/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       int n, k , total=1;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Üssü alınacak sayı : ");
+        n = scanner.nextInt();
+
+        System.out.print("Üs olacak sayı : ");
+         k = scanner.nextInt();
+
+         for (int i = 1;i<=k;i++){
+             total*=n;
+         }
+        System.out.println("Sonuç :"+total);
+    }
+}
